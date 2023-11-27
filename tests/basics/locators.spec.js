ec800e4c5bfa9a3cc0aaa9ac67a7f024458567b0
Filip Meszaros <meszaros.filip@gmail.com>
2023-11-27 17:48:40 +0100

    Add explanation for Playwright locators

---
+++ tests/basics/locators.spec.js
@@ -0,0 +1,20 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+
+/**
+ * Test to show how to use different locators and methods working with locators
+ * By default, Playwright suggests to use CSS selectors, but other selectors are available as well.
+ */
+test('Playwright locators test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  await page.locator('#username').fill('Username CSS locator');
+  await page.locator('[type="password"]').fill('Password CSS locator');
+  await page.locator('div.signInButton').click();
+  const errorText = await page.locator('div[style*="none"]').textContent(); // Returns text of the element
+
+  // If locator is not specific enough and it returns multiple elements, you can specify the one you need
+  await page.locator('.list-elements').first().click();   // Returns  first element from a list of elements and clicks on it
+  await page.locator('.list-elements').nth(1).click();    // Returns second element from a list of elements and clicks on it
+});
